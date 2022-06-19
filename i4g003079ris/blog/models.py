ce6090fcbb7fb7309f6e40e0870d35a66c974a55
Ahire Tersoo <105167338+tersoo-ahire@users.noreply.github.com>
2022-06-19 00:58:52 -0700

    Update models.py

--- i4g003079ris/blog/models.py
+++ i4g003079ris/blog/models.py
@@ -6,7 +6,7 @@ user = get_user_model()
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField(max_length=0)
+    text = models.TextField()
     author = models.ForeignKey(user, on_delete=models.CASCADE)
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
