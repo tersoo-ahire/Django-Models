a9d5773d2eb5ef6b983708757cd55f37e8908708
Ahire Tersoo <105167338+tersoo-ahire@users.noreply.github.com>
2022-06-23 01:07:59 -0700

    Update models.py

--- i4g003079ris/blog/models.py
+++ i4g003079ris/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 user = get_user_model()
 
@@ -8,5 +9,5 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey(user, on_delete=models.CASCADE)
-    created_date = models.DateTimeField()
-    published_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(default=timezone.now)
